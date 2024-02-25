bd87d0c98809500b828d65da9d44edb22694def6
Jeff <144195222+Jeff-is-Deer@users.noreply.github.com>
2024-02-25 09:54:11 -0600

    Update OriginLineDrawing.cs

--- PNGTP_CSharp/MainScenes/OriginLineDrawing.cs
+++ PNGTP_CSharp/MainScenes/OriginLineDrawing.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class OriginLineDrawing : Node2D
 {
+	Line2D Horizontal { get; set; } = null;
+	Line2D Vertical { get; set; } = null;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Horizontal = GetNode<Line2D>("Horizontal");
+		Vertical = GetNode<Line2D>("Vertical");
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void DrawLine()
 	{
+		Vector2 size = GetViewport().GetVisibleRect().Size;
+		Vertical.ClearPoints();
+		Vertical.AddPoint(new Vector2(0.0f , -size.Y));
+		Vertical.AddPoint(new Vector2(0.0f, size.Y));
+		Horizontal.ClearPoints();
+		Horizontal.AddPoint(new Vector2(-size.X , 0.0f));
+		Horizontal.AddPoint(new Vector2(size.X , 0.0f));
 	}
 }
